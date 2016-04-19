419b20d647d6aad7bb7948b3b06b31d6feab49c8
Max MacCamy <maxmac12@gmail.com>
2016-04-18 22:38:46 -0700

    Added support for base page used to browse all produce stored in the database

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from seasonal.views import browse_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', browse_view, name="browse_produce"),
 ]
--- mysite/seasonal/views.py
+++ mysite/seasonal/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from seasonal.models import Produce
 
 # Create your views here.
+
+
+def browse_view(request):
+    produce = Produce.objects.order_by('-name')
+    context = {'produce': produce}
+    return render(request, 'browse.html', context)

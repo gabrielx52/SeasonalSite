a65f64547a4711a5d8be1d97225bf6c9f8b17b25
Max MacCamy <maxmac12@gmail.com>
2016-05-02 13:15:32 -0700

    Adding initial search view and adding zipcode package to requirements.txt

--- mysite/seasonal/views.py
+++ mysite/seasonal/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from seasonal.models import Produce
+from seasonal.models import Produce, Location
 
 # Create your views here.
 
@@ -8,3 +8,10 @@ def browse_view(request):
     produce = Produce.objects.order_by('-name')
     context = {'produce': produce}
     return render(request, 'browse.html', context)
+
+
+def search_view(request):
+    location = Location.objects.order_by('-name')
+    produce = Produce.objects.order_by('-name')
+    context = {'produce': produce, 'location': location}
+    return render(request, 'search.html', context)

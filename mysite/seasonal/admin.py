1694bd40a53f5646fe3d1b3bfdef09864ce831c7
maxmac12 <maxmac12@gmail.com>
2016-05-02 23:13:12 -0700

    Registered Locations model with the admin

--- mysite/seasonal/admin.py
+++ mysite/seasonal/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from seasonal.models import Produce
+from seasonal.models import Produce, Location
 
 # Register your models here.
 admin.site.register(Produce)
+admin.site.register(Location)

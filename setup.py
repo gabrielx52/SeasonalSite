da41c7bb04887a925700b2efc60b8e9ca06ab09d
Max MacCamy <maxmac12@gmail.com>
2016-05-22 16:28:27 -0700

    Added django-extensions and sphinx to the install requires

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-   name='SeasonalSite',
+   name='mysite',
    version='0.0.1',
    author='Max MacCamy, Gabriel Meringolo',
    packages=['mysite'],
@@ -10,7 +10,9 @@ setup(
    long_description=open('README.md').read(),
    install_requires=[
        "Django >= 1.9.5",
+       "django-extensions >= 1.6.7",
        "requests >= 2.9.1",
        "zipcode >= 2.0.0",
+       "Sphinx >= 1.4.1",
    ],
 )

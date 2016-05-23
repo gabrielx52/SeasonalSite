3d6567f100e48b5c22ed1cb6b6ee0ce3af1b48d5
Max MacCamy <maxmac12@gmail.com>
2016-05-23 15:07:10 -0700

    Added docstrings

--- mysite/seasonal/models.py
+++ mysite/seasonal/models.py
@@ -4,6 +4,16 @@ from django.db import models
 
 
 class Produce(models.Model):
+    """
+    Django model representing fruits and vegetables.
+    
+    Attributes:
+        name: A string representation of the object.
+        ideal_temp_low: A decimal representation of the low ideal temperature range(in degrees F).
+        ideal_temp_high: A decimal representation of the high ideal temperature range (in degrees F).
+        growth_time: Integer representation of the grow time (in days).
+        description: A text description of the object.
+    """
     name = models.CharField(max_length=128)
     ideal_temp_low = models.DecimalField(decimal_places=1, max_digits=4)
     ideal_temp_high = models.DecimalField(decimal_places=1, max_digits=4)
@@ -16,10 +26,21 @@ class Produce(models.Model):
         verbose_name_plural = "produce"
 
     def __str__(self):
+        """ String representation of the object."""
         return self.name
 
 
 class Location(models.Model):
+    """
+    Django model representing a location.
+
+    Attributes:
+        zipcode: String representing the zip code.
+        city: String representing the city.
+        state: String representing the state.
+        grow_zone: String representing the grow zones.
+        airport_zone: String representing hte airport zone.
+    """
     zipcode = models.CharField(max_length=5)
     city = models.CharField(max_length=64)
     state = models.CharField(max_length=32)
@@ -31,4 +52,5 @@ class Location(models.Model):
         verbose_name_plural = "locations"
 
     def __str__(self):
+        """ String representation of the object in (city), (state) format."""
         return "{}, {}".format(self.city, self.state)
